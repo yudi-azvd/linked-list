f0419b901fd7d5a45ab056053630f598f3e287ef
Yudi Yamane <yudiyamane@gmail.com>
2019-07-21 21:07:33 -0300

    dando um pulo em branch list pra add create_stack

--- C/core/calculator.c
+++ C/core/calculator.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
-#include "list.h"
+#include "list.h" /* t_stack */
 
 int is_balanced(char expression[]) {
+    int i, expr_lentgh;
+
+    /*
+    clear(char_stack);
+    free(char_stack);
+    */
+
     return 0;
 }
